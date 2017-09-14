fb1def532a3a967e14eef1fa94241a6d235e65fd
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-14 03:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
-  return kittens.unshift('Ralph')
+  kittens.unshift('Ralph')
+  return kittens
 }
